a0f0f462243193420178ae441b059ceb120e0f84
AlexLosciale <losciale17@gmail.com>
2025-02-19 17:53:15 +0100

    add:icon

--- dc-comics/components/Footer.jsx
+++ dc-comics/components/Footer.jsx
@@ -1,46 +1,55 @@
 const Footer = () => {
   return (
     <footer>
-      <div className="footer-links">
-          <div>
-              <h3>DC COMICS</h3>
-              <ul>
-                  <li>Characters</li>
-                  <li>Comics</li>
-                  <li>Movies</li>
-                  <li>TV</li>
-                  <li>Games</li>
-                  <li>Videos</li>
-                  <li>News</li>
-              </ul>
-          </div>
-          <div>
-              <h3>SHOP</h3>
-              <ul>
-                  <li>Shop DC</li>
-                  <li>Shop DC Collectibles</li>
-              </ul>
-          </div>
-          <div>
-              <h3>DC</h3>
-              <ul>
-                  <li>Terms Of Use</li>
-                  <li>Privacy Policy</li>
-                  <li>Ad Choices</li>
-                  <li>Advertising</li>
-                  <li>Jobs</li>
-              </ul>
-          </div>
-      </div>
+        <div className="footer-links">
+            <div>
+                <h3>DC COMICS</h3>
+                <ul>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
+            </div>
+            <div>
+                <h3>SHOP</h3>
+                <ul>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+                </ul>
+            </div>
+            <div>
+                <h3>DC</h3>
+                <ul>
+                    <li>Terms Of Use</li>
+                    <li>Privacy Policy</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                </ul>
+            </div>
+            <div>
+                <h3>SITERS</h3>
+                <ul>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Jobs</li>
+                </ul>
+            </div>
+        </div>
       <button className="signup-btn">SIGN-UP NOW!</button>
-      <div className="socials">
-          <span>FOLLOW US</span>
+        <div className="socials">
           <img src="src/assets/img/footer-facebook.png" alt="facebook" />
           <img src="src/assets/img/footer-twitter.png" alt="twitter" />
           <img src="src/assets/img/footer-youtube.png" alt="youtube" />
           <img src="src/assets/img/footer-pinterest.png" alt="pinterest" />
           <img src="src/assets/img/footer-twitter.png" alt="twitter" />
-      </div>
+        </div>
     </footer>
   )
 };
--- dc-comics/components/Main.jsx
+++ dc-comics/components/Main.jsx
@@ -3,11 +3,26 @@ const Main = () => {
     <main>
       <section className="content">-- Content goes here --</section>
       <section className="promo">
-        <div className="promo-item"><i className="fas fa-book"><img src="src\assets\img\buy-comics-digital-comics.png" alt="digital comic" /></i> DIGITAL COMICS</div>
-        <div className="promo-item"><i className="fas fa-tshirt"><img src="src\assets\img\buy-comics-merchandise.png" alt="merchandise" /></i> DC MERCHANDISE</div>
-        <div className="promo-item"><i className="fas fa-bolt"><img src="src\assets\img\buy-comics-subscriptions.png" alt="subscribe" /></i> SUBSCRIPTION</div>
-        <div className="promo-item"><i className="fas fa-map-marker-alt"><img src="src/assets/img/buy-comics-shop-locator.png" alt="locator" /></i> COMIC SHOP LOCATOR</div>
-        <div className="promo-item"><i className="fas fa-credit-card"><img src="src\assets\img\buy-dc-power-visa.svg" alt="visa" /></i> DC POWER VISA</div>
+        <div className="promo-item">
+          <img src="src\assets\img\buy-comics-digital-comics.png" alt="digital comic"/>
+           <span>DIGITAL COMICS</span>
+        </div>
+        <div className="promo-item">
+          <img src="src\assets\img\buy-comics-merchandise.png" alt="merchandise"/>
+           <span>DC MERCHANDISE</span>
+        </div>
+        <div className="promo-item">
+          <img src="src\assets\img\buy-comics-subscriptions.png" alt="subscribe"/>
+           <span>SUBSCRIPTION</span>
+        </div>
+        <div className="promo-item">
+          <img src="src/assets/img/buy-comics-shop-locator.png" alt="locator"/>
+           <span>COMIC SHOP LOCATOR</span>
+        </div>
+        <div className="promo-item">
+          <img src="src\assets\img\buy-dc-power-visa.svg" alt="visa"/>
+           <span>DC POWER VISA</span>
+        </div>
       </section>
     </main>
   );
