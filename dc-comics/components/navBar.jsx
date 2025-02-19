44b79e55f86c326b88b6b0596fe42b83f1cead77
AlexLosciale <losciale17@gmail.com>
2025-02-19 18:52:23 +0100

    add:arrey

--- dc-comics/components/navBar.jsx
+++ dc-comics/components/navBar.jsx
@@ -1,16 +1,30 @@
 const navbar = () => {
+    const menuLinks = [
+        { id: 1, link: "CHARACTERS", url: "#" },
+        { id: 2, link: "COMICS", url: "#" },
+        { id: 3, link: "MOVIES", url: "#" },
+        { id: 4, link: "TV", url: "#" },
+        { id: 5, link: "GAMES", url: "#" },
+        { id: 6, link: "COLLECTIBLES", url: "#" },
+        { id: 7, link: "VIDEOS", url: "#" },
+        { id: 8, link: "FANS", url: "#" },
+        { id: 9, link: "NEWS", url: "#" },
+        { id: 10, link: "SHOP", url: "#" }
+      ];
+
     return (
         <ul className="menu">
-            <li><a href="#">CHARACTERS</a></li>
-            <li><a href="#">COMICS</a></li>
-            <li><a href="#">MOVIES</a></li>
-            <li><a href="#">TV</a></li>
-            <li><a href="#">GAMES</a></li>
-            <li><a href="#">COLLECTIBLES</a></li>
-            <li><a href="#">VIDEOS</a></li>
-            <li><a href="#">FANS</a></li>
-            <li><a href="#">NEWS</a></li>
-            <li><a href="#">SHOP</a></li>
+            {
+                menuLinks.map((menuLink) => {
+                    return(
+                        <li key ={menuLink.id}>
+                            <a href={menuLink.url}>
+                                {menuLink.link}
+                            </a>
+                        </li>
+                    )
+                })
+            }
         </ul>
     )
 };
